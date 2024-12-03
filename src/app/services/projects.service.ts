82c5c9a09340a8fd71f5a06cdb50665dce344c65
Rolan Muñoz <rolanmunoz.dev@gmail.com>
2024-12-03 22:35:48 +0100

    version@1.0.1

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environment/environment';
+import { environment } from '../../environments/environment';
 import { Category } from '../models/category';
 import { Project } from '../models/project';
 import { Servicio } from '../models/servicio';
